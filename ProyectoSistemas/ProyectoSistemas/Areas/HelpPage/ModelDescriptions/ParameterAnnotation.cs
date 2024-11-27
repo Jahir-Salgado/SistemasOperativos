b17620f4e077ace0ae298356df6dcddd97e0c753
davidum19 <david.um20001@gmail.com>
2024-11-26 22:10:16 -0600

    Proyecto Inicial

---
+++ ProyectoSistemas/ProyectoSistemas/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoSistemas.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
